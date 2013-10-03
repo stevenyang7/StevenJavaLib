42c1dd9971d60dd1563a0dca03f56a509ad91770
steven <stevenyang7@sina.com>
2013-10-03 14:12:36 +0800

    学习 java金额的转移

---
+++ StevenJavaLib/src/com/steven/date/package-info.java
@@ -0,0 +1,15 @@
+/**   
+ * 文件名：package-info.java   
+ *   
+ * 版本信息：V1.0
+ * 日期：2013-10-3   
+ *   
+ */
+
+/**   
+ * 跟时间相关的java知识学习   
+ * @author: 刘壮洪   
+ * @version: 2013-10-3 下午1:36:06    
+ */
+
+package com.steven.date;
---
+++ StevenJavaLib/src/com/steven/money/package-info.java
@@ -0,0 +1,15 @@
+/**   
+ * 文件名：package-info.java   
+ *   
+ * 版本信息：V1.0
+ * 日期：2013-10-3   
+ *   
+ */
+
+/**   
+ * 跟金钱相关的java知识学习
+ * @author: 刘壮洪   
+ * @version: 2013-10-3 下午2:04:48    
+ */
+
+package com.steven.money;
